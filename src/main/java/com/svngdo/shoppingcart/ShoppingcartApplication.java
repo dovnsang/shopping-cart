920b70b77004142c3dd3abd5acd8e1ae53210967
svngdo <svngdo@gmail.com>
2023-09-08 21:20:15 +0700

    initial commit

---
+++ src/main/java/com/svngdo/shoppingcart/ShoppingcartApplication.java
@@ -0,0 +1,13 @@
+package com.svngdo.shoppingcart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingcartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingcartApplication.class, args);
+	}
+
+}
